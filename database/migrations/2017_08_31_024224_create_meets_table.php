188a6a0c5d22e4a976442d1f486cdad02c05fe7a
Chris SIms <creegansims@gmail.com>
2017-09-04 14:46:45 +1200

    first commit

---
+++ database/migrations/2017_08_31_024224_create_meets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMeetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->dateTime('meet_date');
+            $table->string('location');
+            $table->string('lat');
+            $table->string('lng');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
